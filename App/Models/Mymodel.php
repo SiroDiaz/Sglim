0cd02573f06ea5114829c27dca07bf7f50cb8319
Siro_Diaz <siro_diaz@yahoo.com>
2016-04-02 14:53:14 +0200

    Changed indent to whitespaces

--- App/Models/Mymodel.php
+++ App/Models/Mymodel.php
@@ -2,19 +2,19 @@
 namespace App\Models;
 
 class Mymodel {
-	private $db;
+    private $db;
 
-	public function __construct($db) {
-		$this->db = $db;
-	}
+    public function __construct($db) {
+        $this->db = $db;
+    }
 
-	public function getAllUsers() {
-		$query = $this->db->prepare('SELECT * FROM users');
-		$result = $query->execute();
-		if($result) {
-			return $query->fetchAll();
-		}
+    public function getAllUsers() {
+        $query = $this->db->prepare('SELECT * FROM users');
+        $result = $query->execute();
+        if($result) {
+            return $query->fetchAll();
+        }
 
-		return [];
-	}
+        return [];
+    }
 }
--- App/Routes/Api.route.php
+++ App/Routes/Api.route.php
@@ -3,8 +3,8 @@
 use App\Models\Mymodel;
 
 $app->get('/', function($req, $res, $args) {
-	// $mymodel = new Mymodel($this->db);
-	// $users = $mymodel->getAllUsers();
+    // $mymodel = new Mymodel($this->db);
+    // $users = $mymodel->getAllUsers();
     
     return $this->view->render($res, 'index.html', []);
 });
