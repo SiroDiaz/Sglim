a489ec6f052f884a0af515b4c7ec0c4221a85f9f
Siro Díaz Palazón <Siro Daz Palazn>
2016-04-03 23:50:33 +0200

    Vagrant file and environment config file

---
+++ config.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+# Update server
+apt-get update
+apt-get upgrade -y 
+# Install essentials
+apt-get -y install build-essential binutils-doc git -y
+# Install Apache
+apt-get install apache2 -y
+#Install PHP
+apt-get install php5 libapache2-mod-php5 php5-cli php5-mysql -y
+# Install MySQL
+echo "mysql-server mysql-server/root_password password root" | sudo debconf-set-selections
+echo "mysql-server mysql-server/root_password_again password root" | sudo debconf-set-selections
+apt-get install mysql-client mysql-server -y
+# Install PhpMyAdmin
+echo 'phpmyadmin phpmyadmin/dbconfig-install boolean true' | debconf-set-selections
+echo 'phpmyadmin phpmyadmin/app-password-confirm password root' | debconf-set-selections
+echo 'phpmyadmin phpmyadmin/mysql/admin-pass password root' | debconf-set-selections
+echo 'phpmyadmin phpmyadmin/mysql/app-pass password root' | debconf-set-selections
+echo 'phpmyadmin phpmyadmin/reconfigure-webserver multiselect apache2' | debconf-set-selections
+apt-get install phpmyadmin -y
+# Restart Apache service
+service apache2 restart
+# install Composer
+curl -s https://getcomposer.org/installer | php
+mv composer.phar /usr/local/bin/composer
